0daab3b6023dc7f5990eecd4fa24debc9b85e1f4
Garth MacKenzie <garthmac@juno.com>
2015-08-17 22:21:10 -0600

    Rev 0

---
+++ Cassini/DemoURL.swift
@@ -0,0 +1,19 @@
+//
+//  DemoURL.swift
+//  Cassini
+//
+//  Created by iMac21.5 on 4/23/15.
+//  Copyright (c) 2015 Garth MacKenzie. All rights reserved.
+//
+
+import Foundation
+
+struct DemoURL {
+    static let Stanford = NSURL(string: "https://farm6.staticflickr.com/5264/5671465322_27f6027af6_b.jpg")
+    struct NASA {
+        static let Cassini = NSURL(string: "http://www.jpl.nasa.gov/images/cassini/20090202/pia03883-full.jpg")
+        static let Earth = NSURL(string: "http://www.nasa.gov/sites/default/files/wave_earth_mosaic_3.jpg")
+        static let Saturn = NSURL(string: "http://www.nasa.gov/sites/default/files/saturn_collage.jpg")
+    }
+    
+}
---
+++ Cassini/ImageViewController.swift
@@ -0,0 +1,84 @@
+//
+//  ImageViewController.swift
+//  Cassini
+//
+//  Created by iMac21.5 on 4/23/15.
+//  Copyright (c) 2015 Garth MacKenzie. All rights reserved.
+//
+
+import UIKit
+
+class ImageViewController: UIViewController, UIScrollViewDelegate {
+    
+    // this is the model
+    var imageURL: NSURL? {
+        didSet {
+            image = nil
+            if view.window != nil { //check if on screen...never happens
+                fetchImage()
+            }
+        }
+    }
+    
+    @IBOutlet weak var spinner: UIActivityIndicatorView!
+    
+    private func fetchImage() {
+        if let url = imageURL {
+            spinner?.startAnimating()
+            let qos = Int(QOS_CLASS_USER_INITIATED.value)
+            dispatch_async(dispatch_get_global_queue(qos, 0)) { () -> Void in //get .jpg file *slow
+                let imageData = NSData(contentsOfURL: url)
+                dispatch_async(dispatch_get_main_queue()) {
+                    if url == self.imageURL { //mutiple selects...get last
+                        if imageData != nil {
+                            self.image = UIImage(data: imageData!)
+                        } else {
+                            self.image = nil
+                        }
+                    }
+                }
+            }
+        }
+    }
+    @IBOutlet weak var scrollView: UIScrollView! {
+        didSet {  //note: frame.size not image
+            scrollView.contentSize = imageView.frame.size
+            scrollView.delegate = self
+            scrollView.minimumZoomScale = 0.03
+            scrollView.maximumZoomScale = 1.00
+        }
+    }
+    
+    func viewForZoomingInScrollView(scrollView: UIScrollView) -> UIView? {
+        return imageView //this last thing + add ,UIsvDelagate at top
+    }
+    
+    private var imageView = UIImageView()  //no frame yet
+    private var image: UIImage? { // a computed property instead of func
+        get {
+            return imageView.image
+        }
+        set {
+            imageView.image = newValue
+            imageView.sizeToFit()
+            //use ? if not set yet
+            scrollView?.contentSize = imageView.frame.size
+            spinner?.stopAnimating()
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        scrollView.addSubview(imageView)
+        if image == nil && imageURL == nil { //test
+            imageURL = DemoURL.Stanford
+        }
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        if image == nil {
+            fetchImage() // this usually happens
+        }
+    }
+}
